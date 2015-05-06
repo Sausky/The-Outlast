6a56c3b1fcd41b2f5495ccddf33aab69704c8abb
Sausky <735507752@qq.com>
2015-05-06 18:50:02 +0800

    main
    主界面粗略显示以及桌子为范例的点击提示事件

--- app/src/main/java/com/org/outlast/ui/view/MainActivity.java
+++ app/src/main/java/com/org/outlast/ui/view/MainActivity.java
@@ -1,20 +1,39 @@
 package com.org.outlast.ui.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.org.outlast.R;
 
 
 public class MainActivity extends Activity {
+    private ImageView door;
+    private ImageView secret_pic;
+    private ImageView desk;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        door = (ImageView) findViewById(R.id.door);
+        secret_pic = (ImageView) findViewById(R.id.secret_pic);
+        desk = (ImageView)findViewById(R.id.desk);
+        desk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent();
+                intent.setClass(MainActivity.this,desk_prompt.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
