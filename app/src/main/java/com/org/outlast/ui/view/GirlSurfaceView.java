cfe6b70e6a8cac22a86d598967c5badaafe69f98
cat94 <CatMeetPet@gmail.com>
2015-05-13 20:56:59 +0800

    小女孩与部件分开的走动动画
    如题

---
+++ app/src/main/java/com/org/outlast/ui/view/GirlSurfaceView.java
@@ -0,0 +1,110 @@
+package com.org.outlast.ui.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.*;
+import android.view.SurfaceView;
+import android.view.animation.Animation;
+
+import com.org.outlast.R;
+
+
+
+/**
+ * Created by shen on 15/5/9.
+ */
+public class GirlSurfaceView extends SurfaceView implements Callback, Runnable {
+    private SurfaceHolder sfh;
+    private Thread th;
+    private Canvas canvas;
+
+    private int GirlW,GirlH;
+
+    private int controll_flag = 0;
+
+    private Bitmap girl;
+
+    public GirlSurfaceView(Context context){
+        super(context);
+        init();
+    }
+
+    public GirlSurfaceView(Context context, AttributeSet attrs) {     //Constructor that is called when inflating a view from XML
+        super(context,attrs);
+        init();
+
+    }
+    public GirlSurfaceView(Context context, AttributeSet attrs, int defStyle) {     //Perform inflation from XML and apply a class-specific base style
+        super(context,attrs,defStyle);
+        init();
+    }
+
+    public void init() {
+        th = new Thread(this);
+        sfh = this.getHolder();
+        sfh.addCallback(this);
+        setZOrderOnTop(true);    // necessary
+        sfh.setFormat(PixelFormat.TRANSPARENT);
+        this.setKeepScreenOn(true);
+
+
+        girl = BitmapFactory.decodeResource(this.getResources(), R.drawable.girl_small);
+
+        GirlW = girl.getWidth();
+        GirlH = girl.getHeight();
+
+    }
+
+    @Override
+    public void startAnimation(Animation animation) {
+        super.startAnimation(animation);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        th.start();
+    }
+
+    private void draw() {
+
+        int pos_left = controll_flag>1?(controll_flag-2):0;
+
+        int pace = 3;
+
+
+        canvas = sfh.lockCanvas(new Rect(pos_left*3 , 0, GirlW/2 + (pos_left+2)*3, GirlH));
+
+        canvas.save();
+
+
+        canvas.drawColor(Color.TRANSPARENT, android.graphics.PorterDuff.Mode.CLEAR);
+
+        //relative position of the image
+        canvas.drawBitmap(girl, -(controll_flag % 2) * (GirlW / 2)+(pos_left+2)*3, 0, null);
+
+        canvas.restore();
+        sfh.unlockCanvasAndPost(canvas);  // 将画好的画布提交
+    }
+    public void run() {
+        while (controll_flag++<400) {
+            draw();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+    }
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+}
