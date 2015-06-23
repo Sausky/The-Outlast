68bd870d50c40c6225ad3719f924794cee23c6a1
Sausky <735507752@qq.com>
2015-06-23 21:13:04 +0800

    gave

--- app/src/main/java/com/org/outlast/ui/view/mirror_hidden_thing.java
+++ app/src/main/java/com/org/outlast/ui/view/mirror_hidden_thing.java
@@ -4,21 +4,34 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.org.outlast.R;
+import com.org.outlast.UIDecoder;
 
 /**
  * Created by shen on 15/5/31.
  */
 public class mirror_hidden_thing extends Activity {
-    ImageView notebook;
+    ImageView ring;
+    ImageView info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mirror_hidden);
+        ring = (ImageView) findViewById(R.id.portrait);
+        info = (ImageView) findViewById(R.id.info_background);
+        RelativeLayout view = (RelativeLayout) findViewById(R.id.end_lettering);
+        UIDecoder.setBackground(view, getResources(), R.drawable.notebook, 300, 300);
         //获取提示实现点击关闭
-        notebook = (ImageView) findViewById(R.id.notebook);
-        notebook.setOnClickListener(new View.OnClickListener() {
+        ring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                onDestroy();
+            }
+        });
+        info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
